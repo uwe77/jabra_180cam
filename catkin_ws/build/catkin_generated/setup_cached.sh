061984012c252c7d09d125a1c7d16901cd3f8dbd
uwe <uwe90711@gmail.com>
2023-08-25 11:05:29 +0800

    modify usb_cam yml

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/uwe/jabra_180cam/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/uwe/jabra_180cam/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/uwe/jabra_180cam/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/uwe/jabra_180cam/catkin_ws/build'
 export ROSLISP_PACKAGE_DIRECTORIES='/home/uwe/jabra_180cam/catkin_ws/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/uwe/jabra_180cam/catkin_ws/src:$ROS_PACKAGE_PATH"
